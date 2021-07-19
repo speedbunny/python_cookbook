7ee9c0db24b552f457b8382f7cf64c4700f2ea62
Sarah Eaglesfield <speedbunny@gmail.com>
2021-07-19 13:12:22 +0100

    Update and rename loc_replaces.py to locreplace.py

---
+++ text_wrangling/locreplace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 12:17:18 2021
+Text wrangling LOC REPLACES
+@author: Sarah E
+"""
+import pandas as pd
+#Locreplace Function
+def locreplace(df,col,needle,replace,needle2=''):
+   if (needle2==''):
+      df.loc[df[col].str.contains(needle, case=False)==True, col] = replace
+   else:
+     df.loc[[df[col].str.contains(needle, case=False)==True] and df[col].str.contains(needle2, case=False)==True,col] = replace
+
+#In Action
+data = [['granny apple', 'juicy'], ['blood orange', 'refreshing'], ['spanish lemon', 'tangy']]
+fruitdf = pd.DataFrame(data, columns = ['fruit', 'taste'])
+
+locreplace(fruitdf,'fruit','apple','big red apple','granny')
+
+#Single replace
+#locreplace(fruitdf,'fruit','apple','big red nice apple')
+
+#Will fail
+#locreplace(fruitdf,'fruit','apple','big red apple','uncle')
